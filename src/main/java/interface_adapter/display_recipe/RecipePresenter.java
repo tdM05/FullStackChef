68c4ba02955972e658a9d5db04a3965664080698
mousa-sururi1 <mousa.sururi@mail.utoronto.ca>
2024-11-04 20:22:26 -0500

    Presenter

--- src/main/java/interface_adapter/display_recipe/RecipePresenter.java
+++ src/main/java/interface_adapter/display_recipe/RecipePresenter.java
@@ -1,4 +1,67 @@
 package interface_adapter.display_recipe;
 
-public class RecipePresenter {
+/**
+*imports
+ */
+
+/**
+ * The Presenter for the Display_recipe Use Case.
+ */
+
+
+public class RecipePresenter implements NoteOutputBoundary {
+
+    public RecipePresenter(
+
+
+
+
+
+
+}
+
+
+public class LogoutPresenter implements LogoutOutputBoundary {
+
+    private LoggedInViewModel loggedInViewModel;
+    private ViewManagerModel viewManagerModel;
+    private LoginViewModel loginViewModel;
+
+    public LogoutPresenter(ViewManagerModel viewManagerModel,
+                           LoggedInViewModel loggedInViewModel,
+                           LoginViewModel loginViewModel) {
+        this.viewManagerModel = viewManagerModel;
+        this.loggedInViewModel = loggedInViewModel;
+        this.loginViewModel = loginViewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(LogoutOutputData response) {
+        // We need to switch to the login view, which should have
+        // an empty username and password.
+
+        // We also need to set the username in the LoggedInState to
+        // the empty string.
+
+        final LoggedInState loggedInState = loggedInViewModel.getState();
+        loggedInState.setUsername("");
+        this.loggedInViewModel.setState(loggedInState);
+        this.loggedInViewModel.firePropertyChanged();
+
+        final LoginState loginState = loginViewModel.getState();
+        loginState.setUsername("");
+        loginState.setPassword("");
+        this.loginViewModel.setState(loginState);
+        this.loginViewModel.firePropertyChanged();
+
+        // This code tells the View Manager to switch to the LoginView.
+        this.viewManagerModel.setState(loginViewModel.getViewName());
+        this.viewManagerModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+        // No need to add code here. We'll assume that logout can't fail.
+        // Thought question: is this a reasonable assumption?
+    }
 }
