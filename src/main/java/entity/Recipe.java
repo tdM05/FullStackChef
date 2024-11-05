a67f28cbf6be45a43e76ed5f14281898521739e0
Jonathan <johnnny.chung@gmail.com>
2024-11-04 23:47:53 -0500

    Add additional fields to Recipe entity to match Spoonacular API

--- src/main/java/entity/Recipe.java
+++ src/main/java/entity/Recipe.java
@@ -5,11 +5,32 @@ import java.util.List;
 /**
  * The representation of a recipe in our program.
  */
-public interface Recipe {
-    /**
-     * Returns the ingredients needed for this recipe.
-     * @return the ingredients needed for this recipe.
-     */
-    List<Ingredient> getIngredients();
-    NutritionalInfo getNutritionalInfo();
+public class Recipe {
+    private int id;
+    private String title;
+    private String image;
+    private String imageType;
+
+    public Recipe(int id, String title, String image, String imageType) {
+        this.id = id;
+        this.title = title;
+        this.image = image;
+        this.imageType = imageType;
+    }
+
+    // Getters for each field
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public String getImage() {
+        return image;
+    }
+    public String getImageType() {
+        return imageType;
+    }
+
 }
