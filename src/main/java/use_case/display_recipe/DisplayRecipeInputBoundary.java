146b00efeda71b0f82f19608ef582e046511039b
jasmine-artsci <jasmine.jasmine@mail.utoronto.ca>
2024-11-09 12:42:11 -0500

    finished InputBoundary and InputData

--- src/main/java/use_case/display_recipe/DisplayRecipeInputBoundary.java
+++ src/main/java/use_case/display_recipe/DisplayRecipeInputBoundary.java
@@ -1,4 +1,15 @@
 package use_case.display_recipe;
 
+/**
+ * Input boundary for the DisplayRecipe Use Case.
+ * Provides an interface for executing the display recipe functionality.
+ */
 public interface DisplayRecipeInputBoundary {
+
+    /**
+     * Executes the DisplayRecipe use case with the given input data.
+     *
+     * @param inputData The input data containing the recipe ID for displaying.
+     */
+    void execute(DisplayRecipeInputData inputData);
 }
