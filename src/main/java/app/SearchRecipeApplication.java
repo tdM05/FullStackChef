4c2e96daf190b9f02fddeb25d1e337fe5336353c
Jonathan <johnnny.chung@gmail.com>
2024-11-09 16:17:19 -0500

    Completed SearchRecipeApplication

---
+++ src/main/java/app/SearchRecipeApplication.java
@@ -0,0 +1,15 @@
+package app;
+
+import data_access.RecipeDataAccessObject;
+import use_case.search_recipe.SearchRecipeDataAccessInterface;
+
+/**
+ * Builder for the Search Recipe Application.
+ */
+public class SearchRecipeApplication {
+
+
+    public static void main(String[] args) {
+
+    }
+}
