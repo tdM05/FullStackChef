3a6c6bbf79da89f940c70c65634809d38def5fd7
tdM05 <taddmao@gmail.com>
2024-11-08 21:34:27 -0500

    finished recipe interactor

--- src/main/java/use_case/search_recipe/SearchRecipeOutputBoundary.java
+++ src/main/java/use_case/search_recipe/SearchRecipeOutputBoundary.java
@@ -1,6 +1,8 @@
 package use_case.search_recipe;
 
+import java.util.List;
+
 public interface SearchRecipeOutputBoundary {
-    void prepareSuccessView(SearchRecipeOutputData message);
+    void prepareSuccessView(List<SearchRecipeOutputData> message);
     void prepareFailView(String errorMessage);
 }
--- src/main/java/use_case/search_recipe/SearchRecipeOutputData.java
+++ src/main/java/use_case/search_recipe/SearchRecipeOutputData.java
@@ -1,10 +1,28 @@
 package use_case.search_recipe;
 
-import entity.Recipe;
-
-import java.util.List;
-
+/**
+ * The output data interface for the Search Recipe Use Case.
+ */
 public interface SearchRecipeOutputData {
 
-    public List<Recipe> getRecipeName();
+    /**
+     * Gets the name of the recipe.
+     *
+     * @return the recipe name
+     */
+    String recipeName();
+
+    /**
+     * Gets the URL of the recipe image.
+     *
+     * @return the image URL
+     */
+    String imageUrl();
+
+    /**
+     * Gets the ID of the recipe.
+     *
+     * @return the recipe ID
+     */
+    int id();
 }
