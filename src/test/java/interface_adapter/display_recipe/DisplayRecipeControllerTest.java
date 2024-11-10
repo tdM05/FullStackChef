fc24cc5656f10ffed53a13ee2603950034558c89
mousa-sururi1 <mousa.sururi@mail.utoronto.ca>
2024-11-09 21:59:18 -0500

    Updates

---
+++ src/test/java/interface_adapter/display_recipe/DisplayRecipeControllerTest.java
@@ -0,0 +1,18 @@
+package interface_adapter.display_recipe;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DisplayRecipeControllerTest {
+
+    @Test
+    public void execute() {
+        DisplayRecipeController displayRecipeController = new DisplayRecipeController(new DisplayRecipeInputBoundary() {
+            @Override
+            public void execute(DisplayRecipeInputData inputData) {
+                assertEquals(1, inputData.getRecipeId());
+            }
+        });
+        displayRecipeController.execute(1);
+    }
