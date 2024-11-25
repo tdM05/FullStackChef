74244df71c61845ac1876165b11bfbc0ee15d1ee
Jonathan <johnnny.chung@gmail.com>
2024-11-25 03:36:42 -0500

    Complete FavoriteRecipeDataAccessInterface

--- src/main/java/use_case/favorite_recipe/FavoriteRecipeDataAccessInterface.java
+++ src/main/java/use_case/favorite_recipe/FavoriteRecipeDataAccessInterface.java
@@ -1,4 +1,28 @@
 package use_case.favorite_recipe;
 
+import entity.User;
+
+import java.util.List;
+
+/**
+ * The interface of the DAO for the Favorite Recipe Use Case.
+ */
 public interface FavoriteRecipeDataAccessInterface {
+
+
+
+    /**
+     * Returns the list of favorite recipe IDs for the given user.
+     * @param user the user whose favorite recipes are to be retrieved
+     * @return the list of favorite recipe IDs for the given user
+     */
+    List<Integer> getFavorites(User user);
+
+    /**
+     * Updates the system to record the user's favorite recipes.
+     * @param user the user whose favorite recipes are to be saved
+     * @return the list of favorite recipe IDs for the given user
+     */
+    List<Integer> saveFavorites(User user);
+
 }
