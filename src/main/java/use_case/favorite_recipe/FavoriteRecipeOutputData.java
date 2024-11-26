17c76de4e11cf5cf1200e06f3c00409b916d409e
Jonathan <johnnny.chung@gmail.com>
2024-11-26 15:26:05 -0500

    OutputData Complete for User story

--- src/main/java/use_case/favorite_recipe/FavoriteRecipeOutputData.java
+++ src/main/java/use_case/favorite_recipe/FavoriteRecipeOutputData.java
@@ -1,19 +1,18 @@
 package use_case.favorite_recipe;
 
+/**
+ * The Output Data for the Favorite Recipe Use Case.
+ */
 public class FavoriteRecipeOutputData {
     private final boolean favorite;
-    private final boolean useCaseFailed;
 
-    public FavoriteRecipeOutputData(boolean favorite, boolean useCaseFailed) {
+    public FavoriteRecipeOutputData(boolean favorite) {
         this.favorite = favorite;
-        this.useCaseFailed = useCaseFailed;
     }
 
     public boolean getFavorite() {
         return favorite;
     }
 
-    public boolean isUseCaseFailed() {
-        return useCaseFailed;
-    }
+
 }
