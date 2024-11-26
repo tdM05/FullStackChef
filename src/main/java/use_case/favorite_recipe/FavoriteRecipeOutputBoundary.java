af83e569aac0f840e8eeedd9ead18d75a17e9337
Jonathan <johnnny.chung@gmail.com>
2024-11-26 15:16:37 -0500

    OutputBoundary Complete for User story

--- src/main/java/use_case/favorite_recipe/FavoriteRecipeOutputBoundary.java
+++ src/main/java/use_case/favorite_recipe/FavoriteRecipeOutputBoundary.java
@@ -4,6 +4,7 @@ package use_case.favorite_recipe;
  * The output data interface for the Favorite Recipe Use Case.
  */
 public interface FavoriteRecipeOutputBoundary {
+
     /**
      * Prepares the success view for the Favorite Recipe Use Case.
      * @param outputData the output data
