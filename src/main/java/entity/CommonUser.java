5191f6d2ef9c9889ddc924aa448acda993f109f7
Jonathan <johnnny.chung@gmail.com>
2024-11-27 02:30:57 -0500

    Remove Favorites from CommonUser

--- src/main/java/entity/CommonUser.java
+++ src/main/java/entity/CommonUser.java
@@ -8,19 +8,11 @@ import java.util.ArrayList;
 public class CommonUser implements User {
     private final String name;
     private String password;
-    private final Favorite favorite;
 
     // Delete later because the notes application needs it for now but it will be deleted later so please ignore it
     public CommonUser(String name, String password) {
         this.name = name;
         this.password = password;
-        this.favorite = new CommonFavorite(new ArrayList<>());
-    }
-
-    public CommonUser(String name, String password, Favorite favorite) {
-        this.name = name;
-        this.password = password;
-        this.favorite = favorite;
     }
 
     @Override
@@ -38,8 +30,4 @@ public class CommonUser implements User {
         this.password = password;
     }
 
-    @Override
-    public Favorite getFavorite() {
-        return favorite;
-    }
 }
