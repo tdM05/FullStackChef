c2313a95789fc5a95613e77fcb49f4faa4fb0fb8
Jonathan <johnnny.chung@gmail.com>
2024-11-27 04:04:58 -0500

    Using Singleton pattern to create SessionUser

---
+++ src/main/java/app/SessionUser.java
@@ -0,0 +1,34 @@
+package app;
+
+import entity.User;
+
+/**
+ * The Session User.
+ */
+public class SessionUser {
+    private static SessionUser instance;
+    private User user;
+
+    private SessionUser() {
+
+    }
+
+    public static SessionUser getInstance() {
+        if (instance == null) {
+            instance = new SessionUser();
+        }
+        return instance;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void clearUser() {
+        this.user = null;
+    }
+}
