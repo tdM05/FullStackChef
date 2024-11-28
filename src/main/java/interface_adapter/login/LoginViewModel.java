202b709dbd2a118257be7b3443d30e27e6042a0e
Jonathan <johnnny.chung@gmail.com>
2024-11-28 16:50:58 -0500

    Login Use Case Complete

--- src/main/java/interface_adapter/login/LoginViewModel.java
+++ src/main/java/interface_adapter/login/LoginViewModel.java
@@ -7,7 +7,7 @@ import interface_adapter.ViewModel;
  */
 public class LoginViewModel extends ViewModel<LoginState> {
     public LoginViewModel(){
-        super("log in");
+        super("logInView");
         setState(new LoginState());
     }
 }
--- src/main/java/use_case/login/LoginInteractor.java
+++ src/main/java/use_case/login/LoginInteractor.java
@@ -1,5 +1,6 @@
 package use_case.login;
 
+import app.SessionUser;
 import entity.User;
 
 /**
@@ -31,7 +32,11 @@ public class LoginInteractor implements LoginInputBoundary {
             }
             else {
                 final User user = userDataAccessObject.get(loginInputData.getUsername());
+
                 userDataAccessObject.setCurrentUsername(user.getName());
+
+                // Set the SessionUser instance
+                SessionUser.getInstance().setUser(user);
 
                 final LoginOutputData loginOutputData = new LoginOutputData(user.getName(), false);
                 loginPresenter.prepareSuccessView(loginOutputData);
